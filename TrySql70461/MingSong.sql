f4c85acd19d4474711fa0d6f478b9aa50ab8e13e
walterhuang <isponder@gmail.com>
2014-03-18 14:21:19 +0800

    pilot

---
+++ TrySql70461/MingSong.sql
@@ -0,0 +1,56 @@
+﻿--Member存放會員資料
+--Song存放歌曲記錄
+--MemberSong存放會員點歌記錄,會員編號與歌曲編號合起來不重複
+
+--請用T-SQL產生"每個會員必點的歌曲"
+
+--a
+--Member(MID,Password,MName)
+
+--create table Member (MID int not null primary key , MName varchar(10))
+
+--insert Member values (1,'John')
+--insert Member values (2,'Mary')
+--insert Member values (3,'Tom')
+
+----Song (SID,SName)
+
+--create table Song(SID int not null primary key , SName nvarchar(10))
+----drop table Song
+--insert Song values (1,'星星')
+--insert into Song values (2,N'月亮')
+--insert Song values (3,N'太陽')
+
+----MemberSong(MID,SID)
+
+--create table MemberSong(MID int , SID int)
+
+--insert MemberSong values (1,1),(1,2),(1,3),(2,1),(3,1)
+--insert membersong values(2,3)
+--select * from Member 
+--select * from Song 
+
+select * from MemberSong 
+--insert membersong values(2,2)
+-- insert membersong values(3,2)
+select sid
+from membersong
+group by sid 
+having count(*) = (select count(*) from member)
+
+select Song.SID,Song.SName 
+from Song inner join
+(Select Song.SID,COUNT(MemberSong.SID) as count 
+from Song inner join MemberSong on Song.SID = MemberSong.SID
+group by Song.SID
+having COUNT(MemberSong.SID)=(Select COUNT(*) from Member)
+)
+as a 
+on Song.SID = a.SID
+
+--select * 
+--from membersong A
+--join membersong B on A.MID <> B.MID and A.SID = B.SID
+--order by 1, 2
+
+
---
+++ TrySql70461/byronhuexam.sql
@@ -0,0 +1,73 @@
+﻿
+-- this syntax is awesome
+select * from (values(1),(2)) t(c1)
+select * from (values(1,'A'), (2, 'B'), (3, 'C')) t(c1, c2)
+
+-- q3
+select 1+'1'
+-- q4
+select avg(c1) from (values(2),(null), (4)) t(c1)
+-- q5
+select cast(3 as decimal)/1000
+
+
+-- Q1
+select * from (values(1),(2)) t(c1) 
+union 
+select * from (values(2),(3)) t2(c1)
+-- Q2
+begin tran
+begin tran
+rollback
+if @@ERROR=0 select @@TRANCOUNT
+-- Q3
+select rank() over(order by c) ,
+ROW_NUMBER()  over(order by c),
+DENSE_RANK()  over(order by c)
+from(values(1),(1),(2),(2)) T(c)
+-- Q4
+select * from(values(1))t(c1) for xml auto
+
+
+-- Q5
+create table t1(c1 nvarchar(100), c2 nvarchar(100))
+go
+select * into t2 from t1 where 1=0
+go
+create trigger tr2 on t1
+instead of update
+as
+declare @int varchar(100)
+select @int =c1 from deleted
+update t1 set c1=a.c1, c2=a.c1+'XXX'
+from inserted a where t1.c1 = @int
+go
+insert t1 values(N'a', N'a')
+insert t1 values(N'b', N'b')
+update t1 set c1=N'aa', c2=N'abcd' where c1=N'a'
+select * from t1
+drop table t1
+drop table t2
+-- Q12
+create table tb(PK int primary key)
+go
+begin tran
+  insert tb values(1)
+  begin tran
+    insert tb values(2)
+  commit
+rollback
+go
+select * from tb
+drop table tb
+
+-- Q13
+create table tb(PK int primary key)
+go
+begin tran
+insert tb values(1)
+insert tb values(1)
+commit tran
+go
+select * from tb
+drop table tb
